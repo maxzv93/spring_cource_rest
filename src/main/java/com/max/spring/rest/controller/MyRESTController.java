c3fe55c4fe7e6241a29ad89f7623cb39e881b20c
ZuevMYu <maxzv93@gmail.com>
2024-11-04 23:50:45 +0300

    addUpdateDelete

--- src/main/java/com/max/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/max/spring/rest/controller/MyRESTController.java
@@ -32,12 +32,37 @@ public class MyRESTController {
 
     @GetMapping("/employees/{id}")
     public Employee shawAllEmployee(@PathVariable int id) {
+        Employee employee = getEmployeeWithException(id);
+
+        return employee;
+    }
+
+    private Employee getEmployeeWithException(int id) {
         Employee employee = employeeService.getEmployee(id);
         if (employee == null) {
             throw new NoSuchEmployeeException("There is no employee with id = " + id + " in Database");
         }
-
         return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id){
+        getEmployeeWithException(id);
+        
+        employeeService.deleteEmployee(id);
+        return "Employee with id = " + id + " was deleted";
     }
 }
 
